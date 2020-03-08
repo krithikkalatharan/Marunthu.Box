68e4513358bffa00a32043427c2f26b2024b56aa
krithikkalatharan <krithik2501@gmail.com>
2020-03-08 12:28:01 +0530

    master

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var http = require('http');
 function onRequest(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write('Matunthu.Box');
-    response.write('An online platform for medicines');
     response.end();
 }
 
